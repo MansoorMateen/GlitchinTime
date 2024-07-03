3218330c8291d932a8675c3a8775a0acc06f6883
MansoorMateen <71268008+MansoorMateen@users.noreply.github.com>
2024-07-03 11:25:50 +0200

    added the unity code

---
+++ Assets/scripts/ItemProperties.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class ItemProperties : MonoBehaviour
+{
+    public float growthFactor = 1.1f;
+    public float speedIncrement = 0.5f;
+    public AudioClip collectSound;
+}
